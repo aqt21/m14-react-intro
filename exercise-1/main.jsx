5dfb25382d578afebc0b4c35bc4b6a383ba6d243
Andrew Tran <andrewtran97@gmail.com>
2016-11-01 10:13:48 -0700

    classwork

--- exercise-1/main.jsx
+++ exercise-1/main.jsx
@@ -1,11 +1,20 @@
 // Main.jsx file
 
 // Create a simple component
+var MyComponent = React.createClass({
     // In your render function...
+    render:function() {
         // Set variables `name` and `interest`
 
-
         // Return div with two paragraphs
-
+        return(
+        	<div>
+        		<p>Hi my name is {this.props.name}</p>
+        		<p>I am interested in {this.props.interest}</p>
+    		</div>
+        )
+    }
+});
 
 // Render your component in the `main` section
+ReactDOM.render(<MyComponent name="Andrew Tran" interest="Bouldering"/>, document.querySelector("main"));
